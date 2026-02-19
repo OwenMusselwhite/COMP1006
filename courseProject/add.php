02c45665fb9875877dc1595c2bf47e35a23c7b04
OwenMusselwhite <owenmusselwhite07@gmail.com>
2026-02-19 15:20:36 -0500

    gonna just keep working and try to fix page later, added layout for add.php

---
+++ courseProject/add.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Add Player</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <h1>Add New Player</h1>
+
+</body>
+</html>
