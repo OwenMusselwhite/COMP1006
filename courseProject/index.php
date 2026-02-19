665d1149addfc012b3ecdf1aca52e993534bce9a
OwenMusselwhite <owenmusselwhite07@gmail.com>
2026-02-19 14:07:07 -0500

    deleted everything and rebuilding index, starting with messages for succesful add, delete, update

--- courseProject/index.php
+++ courseProject/index.php
@@ -4,6 +4,8 @@ require "includes/connect.php"; //connect to the database
 //fetch all the players from the database
 $stmt = $pdo->prepare("SELECT * FROM roster ORDER BY last_name DESC"); //select all players from the database and order by last name
 $players = $stmt->fetchAll(PDO::FETCH_ASSOC); //fetch all the players
+
+$success = $_GET['success'] ?? null; //get the success message from the URL
 ?>
 
 <!DOCTYPE html>
@@ -13,32 +15,29 @@ $players = $stmt->fetchAll(PDO::FETCH_ASSOC); //fetch all the players
     <link rel="stylesheet" href="css/main.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Course-Project</title>
+    <title>Team Roster</title>
 </head>
 
 <body>
 
-    <main class="container mt-4">
-        <h1>Team Roster</h1>
+    <main class="container mt-4"> 
+        <h1 class="Main-Header">Team Roster</h1>
 
-        <form action="process.php" method="post" class="mt-3">
-            <label class="form-label" for="first_name">First Name</label>
-            <input class="form-control" type="text" id="first_name" name="first_name">
+        <?php if ($success === "added"): ?>
+        <div class="success">Player added successfully.</div>
 
-            <label class="form-label mt-3" for="last_name">Last Name</label>
-            <input class="form-control" type="text" id="last_name" name="last_name">
+        <?php elseif ($success === "updated"): ?>
+        <div class="success">Player updated successfully.</div>
 
-            <label class="form-label mt-3" for="email">Email Address</label>
-            <input class="form-control" type="email" id="email" name="email">
+        <?php elseif ($success === "deleted"): ?>
+        <div class="success">Player deleted successfully.</div>
+    <?php endif; ?>
 
-            <label class="form-label mt-3" for="position">Position</label>
-            <input class="form-control" type="text" id="position" name="position">
 
-            <label class="form-label mt-3" for="phone">Phone Number</label>
-            <input class="form-control" type="tel" id="phone" name="phone">
-
-            <button class="btn btn-primary mt-4" type="submit">Add to Roster</button>
-        </form>
+<div class="top-actions">
+    <a class="btn" href="add.php">Add New Player</a>
+</div>
+        
 
 
 
