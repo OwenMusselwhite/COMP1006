8f37bc7792aa915e6c882c8e8822411600656a8b
OwenMusselwhite <owenmusselwhite07@gmail.com>
2026-04-10 12:09:11 -0400

    added the recaptcha script in the headers

--- courseProject/includes/header.php
+++ courseProject/includes/header.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Roster </title>
     <link href="styles/main.css" rel="stylesheet">
+    <script src="https://www.google.com/recaptcha/api.js"></script>
 </head>
 
 <body>
--- courseProject/includes/header_admin.php
+++ courseProject/includes/header_admin.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Player Portal</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link href="styles/main.css" rel="stylesheet">
-</head>
+    <link href="styles/main.css" rel="stylesheet">    
+    <script src="https://www.google.com/recaptcha/api.js"></script></head>
 
 <body>
     <header>
