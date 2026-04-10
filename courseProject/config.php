6deee262362db870328444c0af31dc83b7378233
OwenMusselwhite <owenmusselwhite07@gmail.com>
2026-04-10 12:03:11 -0400

    finished config.php

--- courseProject/config.php
+++ courseProject/config.php
@@ -19,5 +19,15 @@ function verifyRecaptcha($response) {
             'content' => http_build_query($data)
         ]
     ];
+    
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
 
+    if ($result === false) {
+        return false;
+    }
+
+    $result = json_decode($result, true);
+    return $result['success'] ?? false;
+}
 ?>
