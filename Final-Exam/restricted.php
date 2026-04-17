de4e887feb51ab5878e5b8e15e079b4966ccb4ad
OwenMusselwhite <owenmusselwhite07@gmail.com>
2026-04-17 12:35:46 -0400

    made restricted.php

---
+++ Final-Exam/restricted.php
@@ -0,0 +1,9 @@
+
+<body>
+    <main class="container restricted text-center">
+        <h1> Sorry, you must be logged into view this content! </h1>
+        <a href="index.php" class="btn btn-primary"> Back to Home Page </a>
+    </main>
+</body>
+
+</html>
